fcdebc7b8cc16b59daa8608d2b66b5229d548b4c
lucasU1969 <lromanoabdo@udesa.edu.ar>
2023-09-17 11:02:41 -0300

    update

---
+++ TP2_Queue/src/queue/SlotWithElement.java
@@ -0,0 +1,14 @@
+package queue;
+
+public class SlotWithElement extends Slot{
+    private Object element;
+    public SlotWithElement(Object cargo) {
+    	element = cargo;
+    }
+    public boolean isEmpty() {
+        return false;
+    }
+    public Object getElement() {
+        return element;
+    }
+}
