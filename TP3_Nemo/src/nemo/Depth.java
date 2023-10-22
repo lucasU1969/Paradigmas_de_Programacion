96743d1bd5347961d0c80d19935b4708b4237060
lucasU1969 <lromanoabdo@udesa.edu.ar>
2023-10-22 18:16:07 -0300

    updates

--- TP3_Nemo/src/nemo/Depth.java
+++
@@ -1,8 +0,0 @@
-package nemo;
-
-public abstract class DepthOperator {
-    public abstract Depth moveUpwards();
-    public abstract Depth moveDownwards();
-    public abstract void launchCapsule(Nemo submarine);
-    public abstract int getDepth();
-}
